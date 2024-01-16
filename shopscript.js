1cc799fdbf48cf6110c6c8e87911e6f5a272b008
fredrikkri <fjkri@hotmail.com>
2024-01-16 18:10:11 +0100

    gjort endringer, fått inn mer design

--- shopscript.js
+++ shopscript.js
@@ -1,1 +1,17 @@
-document.getElementById("test").innerHTML = "Det fungerer!"
+console.log(products)
+
+let productsHTML = "<h2>NINJAGO</h2>"
+
+products.map(product => productsHTML += 
+`<article>
+<img src="website_images/PROD_${product.imagefile}" alt="${product.title}">
+<a href="#">${product.category}</a>
+<h3>${product.title}</h3>
+<span>Kr. ${product.price}</span>
+<button>Legg i handlekurv</button>
+</article>`)
+
+console.log(productsHTML)
+
+const main = document.getElementsByTagName("main")
+main[0].innerHTML = productsHTML
