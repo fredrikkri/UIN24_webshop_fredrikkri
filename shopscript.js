703a2b08ba402fdee9f0cc929bfc5e5aa6bc95da
fredrikkri <fjkri@hotmail.com>
2024-01-18 09:59:37 +0100

    fiksa cart

--- shopscript.js
+++ shopscript.js
@@ -17,27 +17,66 @@ const main = document.getElementsByTagName("main")
 main[0].innerHTML = productsHTML
 
 // HANDLEVOGNFUNKSJONALITET
+
+document.getElementById("carttoggle").addEventListener("click", function() {
+    const cart = document.getElementById("cart")
+    cart.classList.toggle("show")
+})
+
 function addToCart(productid) {
     // console.log("addToCart kjører:" + productid)
-    cart.push({product: productid, quantity: 1})
+
+    let exist = cart.findIndex(p => productid === p.product)
+    console.log("Exist: " + exist)
+
+    if(exist === -1) {
+        cart.push({product: productid, quantity: 1})
+    } else {
+        cart[exist].quantity += 1
+    }
+    
     console.log(cart)
 
     updateCartDisplay()
 }
 
 function updateCartDisplay() {
+
+    let cartCount = 0
+
+    cart.map(p => cartCount += p.quantity)
+    document.getElementById("cartcount").innerHTML = cartCount
+
     let cartHTML = ""
-    cart.map(prod => {
-        let filteredProduct = products.filter(filterprod => prod.product === filterprod.prodid)
-        console.log(filteredProduct)
-        cartHTML += `<li>
-            <span class="title">${filteredProduct[0].title}</span>
-            <span class="price">${filteredProduct[0].price},-</span>
-            <span class="quantity">X${prod.quantity}</span>
-            <span class="functions">
-                <button>X</button>
-            </span>
-        </li>`
-    })
+
+    if(cart.length === 0) {
+        cartHTML += "<li>Du har ingen produkter i handlevognen din."
+    } else {
+        cart.map((prod, index) => {
+            let filteredProduct = products.filter(filterprod => prod.product === filterprod.prodid)
+            console.log(filteredProduct)
+            cartHTML += `<li>
+                <span class="title">${filteredProduct[0].title}</span>
+                <span class="price">${filteredProduct[0].price},-</span>
+                <span class="quantity">X${prod.quantity}</span>
+                <span class="functions">
+                    <button onclick="removeFromCart(${index})">X</button>
+                </span>
+            </li>`
+        })
+    }
     document.getElementById("cartlist").innerHTML = cartHTML
 }
+
+function removeFromCart(index) {
+    console.log("Removing " + index)
+
+    if(cart[index].quantity > 1) {
+        cart[index].quantity -= 1
+    } else {
+        cart.splice(index, 1)
+    }
+    updateCartDisplay()
+}
+
+updateCartDisplay()
